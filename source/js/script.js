e8b350d6accd2950eba37bdfe037e9b600b3173e
Ivan Muliarchyk <Ivan.Muliarchik@fidectus.com>
2020-08-24 22:46:46 +0300

    добавляет анимацию появления шапки, лого, футера

--- source/js/script.js
+++ source/js/script.js
@@ -8,6 +8,7 @@ import result from './modules/result.js';
 import form from './modules/form.js';
 import social from './modules/social.js';
 import FullPageScroll from './modules/full-page-scroll';
+import pageLoaded from './modules/page-loaded.js';
 
 // init modules
 mobileHeight();
@@ -18,6 +19,7 @@ chat();
 result();
 form();
 social();
+pageLoaded();
 
 const fullPageScroll = new FullPageScroll();
 
