20de9b69e68af08e323faeaa4b60cc723c7f59bc
Ivan Muliarchyk <Ivan.Muliarchik@fidectus.com>
2020-08-27 21:25:35 +0300

    правит анимацию фона

--- source/js/modules/animation-bar.js
+++ source/js/modules/animation-bar.js
@@ -1,22 +1,26 @@
 export default () => {
   const animationBar = document.querySelector(`.animation-bar`);
   const menu = Array.from(document.querySelectorAll(`.js-menu-link`));
-  const animationScreen = document.querySelector(`.animation-screen`);
+  const prizes = document.querySelector(`.screen--prizes`);
+  const story = document.querySelector(`.screen--story`);
 
+
+  animationBar.addEventListener(`transitionend`, () => {
+    story.classList.remove(`transtioning`);
+    story.classList.add(`screen--hidden`);
+    story.classList.remove(`active`);
+
+    animationBar.classList.remove(`active`);
+
+    prizes.classList.remove(`screen--hidden`);
+    prizes.classList.add(`active`);
+  });
 
   menu.forEach((elem) => {
     elem.addEventListener(`click`, function () {
-      if (elem.dataset.href === `prizes`) {
-        animationScreen.style.display = `none`;
-
-        animationBar.addEventListener(`transitionend`, () => {
-          animationBar.classList.remove(`active`);
-        });
-
-        animationBar.classList.add(`active`);
-      } else if (elem.dataset.href !== `prizes`) {
-        animationScreen.style.display = `flex`;
-        animationBar.classList.remove(`active`);
+      if (elem.dataset.href !== `prizes`) {
+        prizes.classList.add(`screen--hidden`);
+        prizes.classList.remove(`active`);
       }
     });
   });
