bb63d6625dd8836581aaeca9d627f6400e019116
Ivan Muliarchyk <Ivan.Muliarchik@fidectus.com>
2020-11-16 00:07:05 +0300

    Правит инициализацию анимации

--- source/js/modules/seacalf-scene/index.js
+++ source/js/modules/seacalf-scene/index.js
@@ -7,47 +7,48 @@ export default () => {
   const ANIMATION_DURATION = 1400;
   const TOTAL_DURATION = 3000;
 
+  const canvasElement = document.getElementById(`seacalf-canvas`);
   let animate = false;
+  let loadSeacalfCount = 0;
+
+  canvasElement.width = window.innerWidth;
+  canvasElement.height = window.innerHeight;
+
+  const ctx = canvasElement.getContext(`2d`);
+
+  const seacalf = new Seacalf({duration: ANIMATION_DURATION, ctx});
+  seacalf.initImages();
+
+  const snowflakeLeft = new Snowflake({
+    duration: ANIMATION_DURATION,
+    ctx,
+    position: {
+      top: window.innerHeight / 2 + 20,
+      left: window.innerWidth / 2 - 370,
+    },
+    skew: [-0.3, 0.3],
+    scale: [1, 1],
+  });
+  snowflakeLeft.initImage();
+
+  const snowflakeRight = new Snowflake({
+    duration: ANIMATION_DURATION,
+    ctx,
+    position: {
+      top: window.innerHeight / 2 + 80,
+      left: window.innerWidth / 2 + 300,
+    },
+    skew: [0.3, -0.3],
+    scale: [-0.7, 0.7],
+  });
+  snowflakeRight.initImage();
+
+  const plane = new Plane({duration: ANIMATION_DURATION, ctx});
+  plane.initImages();
 
   const drawScene = () => {
     if (!animate) {
       animate = true;
-
-      const canvasElement = document.getElementById(`seacalf-canvas`);
-      canvasElement.width = window.innerWidth;
-      canvasElement.height = window.innerHeight;
-
-      const ctx = canvasElement.getContext(`2d`);
-
-      const seacalf = new Seacalf({duration: ANIMATION_DURATION, ctx});
-      seacalf.initImages();
-
-      const snowflakeLeft = new Snowflake({
-        duration: ANIMATION_DURATION,
-        ctx,
-        position: {
-          top: window.innerHeight / 2 + 20,
-          left: window.innerWidth / 2 - 370,
-        },
-        skew: [-0.3, 0.3],
-        scale: [1, 1],
-      });
-      snowflakeLeft.initImage();
-
-      const snowflakeRight = new Snowflake({
-        duration: ANIMATION_DURATION,
-        ctx,
-        position: {
-          top: window.innerHeight / 2 + 80,
-          left: window.innerWidth / 2 + 300,
-        },
-        skew: [0.3, -0.3],
-        scale: [-0.7, 0.7],
-      });
-      snowflakeRight.initImage();
-
-      const plane = new Plane({duration: ANIMATION_DURATION, ctx});
-      plane.initImages();
 
       const render = () => {
         ctx.save();
@@ -72,10 +73,47 @@ export default () => {
         () => snowflakeRight.animate()
       ]);
 
-
       animateDuration(render, TOTAL_DURATION);
     }
   };
 
-  drawScene();
+  const init = () => {
+    const increaseLoadSeacalfImg = () => {
+      loadSeacalfCount++;
+
+      if (loadSeacalfCount === 7) {
+        drawScene();
+      }
+    };
+
+    seacalf.seacalf.img.onload = () => {
+      increaseLoadSeacalfImg();
+    };
+
+    seacalf.ice.img.onload = () => {
+      increaseLoadSeacalfImg();
+    };
+
+    plane.plane.img.onload = () => {
+      increaseLoadSeacalfImg();
+    };
+
+    plane.leftTree.img.onload = () => {
+      increaseLoadSeacalfImg();
+    };
+
+    plane.rightTree.img.onload = () => {
+      increaseLoadSeacalfImg();
+    };
+
+    snowflakeLeft.snowflake.img.onload = () => {
+      increaseLoadSeacalfImg();
+    };
+
+    snowflakeRight.snowflake.img.onload = () => {
+      increaseLoadSeacalfImg();
+    };
+  };
+
+  init();
 };
